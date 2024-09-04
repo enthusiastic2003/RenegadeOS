40d5c11f34f241482bd3220021c0c303077486fb
Sirjan Hansda <sirjanh@Sirjans-MacBook-Pro.local>
2024-09-03 18:36:19 -0700

    Passed meminfo to bootmain. Modules left to handle

--- src/bootstrapper/bootmain.c
+++ src/bootstrapper/bootmain.c
@@ -1,7 +1,20 @@
 #include "stdio.h"
+#include "multiboot2.h"
 
-
-void bootmain(void) {
+void bootmain(unsigned long magic, unsigned long addr) {
     bprintf("This is a 32-bit bootstrapper.\n");
+    bprintf("Magic number: %x\n", magic);
+    struct multiboot_tag *tag;
+    unsigned size;
+    if (magic != MULTIBOOT2_BOOTLOADER_MAGIC) {
+        bprintf("Invalid magic number. Halting...\n");
+        for(;;);
+    }
+  if (addr & 7)
+    {
+      bprintf ("Unaligned mbi: 0x%x\n", addr);
+      return;
+    }
     
+    for(;;);
 }
